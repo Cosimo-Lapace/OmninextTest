2b3976f522062ed7af11606f01ba58c0cb8c018f
CosimoLP <cosimolapace@libero.it>
2023-06-08 10:38:41 +0200

    header

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, DoCheck, OnInit, } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+  isSmallScreen: boolean = false;
+  constructor() {}
+
+
+
+}
