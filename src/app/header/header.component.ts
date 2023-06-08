d2f8637938f71aad43b4909237905768001fb622
CosimoLP <cosimolapace@libero.it>
2023-06-08 10:56:33 +0200

    ngfor

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,19 @@ import { Component, DoCheck, OnInit, } from '@angular/core';
 })
 export class HeaderComponent {
 
-  isSmallScreen: boolean = false;
+  CheckIcon: boolean = false;
+  c = 0;
+  dropdownItems: string[] = [' Utenti illimitati', '  Piattaforma MySarma', ' Gestione del contratto online','  MySarma Welfare Card (fino a 258€)',' Importi personalizzabili online',' Sconti e Coupon']
   constructor() {}
+  checkicon() {
+    this.CheckIcon = true;
+    this.c++;
+    if(this.c === 2){
+      this.CheckIcon = false;
+      this.c= 0;
+    }
 
+  }
 
 
 }
